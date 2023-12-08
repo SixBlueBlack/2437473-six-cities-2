939efd94070d819506555fc382fe074684753875
SBB <me.mashkin7@gmail.com>
2023-12-08 15:50:39 +0500

    Приложение для терминала. Часть 2

--- src/core/cli-command/import.command.ts
+++ src/core/cli-command/import.command.ts
@@ -1,21 +1,32 @@
 import {CliCommandInterface} from './cli-command.interface.js';
 import TSVFileReader from '../file-reader/tsv-file-reader.js';
 import chalk from 'chalk';
+import {createOffer} from '../helpers/offers.js';
+import {getErrorMessage} from '../helpers/common.js';
 
 export default class ImportCommand implements CliCommandInterface {
   public readonly name = '--import';
 
-  public execute(filename: string): void {
+  private onLine(line: string) {
+    const offer = createOffer(line);
+    console.log(offer);
+  }
+
+  private onComplete(count: number) {
+    console.log(`${count} rows imported`);
+  }
+
+  public async execute(filename: string): Promise<void> {
     const fileReader = new TSVFileReader(filename.trim());
 
+    fileReader.on('line', this.onLine);
+    fileReader.on('end', this.onComplete);
+
     try {
-      fileReader.read();
-      console.log(fileReader.toArray());
+      await fileReader.read();
     } catch (err) {
-      if (!(err instanceof Error)) {
-        throw err;
-      }
-      console.log(`Не удалось импортировать данные из файла по причине: "${chalk.red(err.message)}"`);
+      console.log(`Can't read the file: "${chalk.red(getErrorMessage(err))}"`);
     }
   }
 }
+
